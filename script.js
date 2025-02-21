2f75e9ec4ef5e0f38a816cb77c8e729a88ed9e90
gabrielabade <gabriela.bade@hotmail.com>
2025-02-21 18:05:48 -0300

    aprimora a funcionalidade de exibição de conteúdo e melhora a descrição e título da página inicial

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function toggleContent(className) {
-    var content = document.querySelector('.' + className);
-    if (content.style.display === 'none' || content.style.display === '') {
-      content.style.display = 'block';
+
+// Função para abrir e fechar as seções de informações
+document.addEventListener("DOMContentLoaded", function () {
+  function toggleContent(section) {
+    let content = document.querySelector(`.${section}`);
+    let icon = document.querySelector(`.${section}`).previousElementSibling.querySelector("ion-icon");
+
+    if (content.style.display === "none" || content.style.display === "") {
+      content.style.display = "block";
+      icon.setAttribute("name", "chevron-up-outline");
     } else {
-      content.style.display = 'none';
+      content.style.display = "none";
+      icon.setAttribute("name", "chevron-down-outline");
     }
   }
+
+  document.querySelectorAll(".information").forEach((element) => {
+    element.addEventListener("click", function () {
+      let sectionClass = this.nextElementSibling.classList[1]; // Obtém a classe específica do conteúdo associado
+      toggleContent(sectionClass);
+    });
+  });
+});
